de63bc3a6ab4a0f5618fe5891b5d3372231c5d32
dpfg <xander.blr@gmail.com>
2021-02-16 21:30:39 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import tinycolor from "tinycolor2";
+import { base, charts, showcase, saturated } from "./input.js";
+
+function distance(c1, c2) {
+  const c1hsl = c1.toHsl();
+  const c2hsl = c2.toHsl();
+
+  const v1 = [c1hsl.h, c1hsl.s, c1hsl.l, c1hsl.a];
+  const v2 = [c2hsl.h, c2hsl.s, c2hsl.l, c2hsl.a];
+
+  let i,
+    d = 0;
+
+  for (i = 0; i < v1.length; i++) {
+    d += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+  }
+  return Math.sqrt(d);
+}
+
+function findMatches(color) {
+  return showcase.filter((e) => {
+    const sc = tinycolor(e.color);
+    return distance(color, sc) < 1;
+  });
+}
+
+function newScale(color, name) {
+  const sourceColor = tinycolor(color);
+  const sourceColorHSL = sourceColor.toHsl();
+
+  const sourceLightness = sourceColorHSL.l; // 8 * Math.round((sourceColorHSL.l * 100) / 8);
+
+  const scale = [];
+
+  for (let step = 10; step < 100; step += 10) {
+    const lightness =
+      Math.round(sourceLightness + (step - sourceLightness)) / 100;
+
+    const blockColor = tinycolor.fromRatio({
+      h: sourceColorHSL.h,
+      s: sourceColorHSL.s,
+      l: lightness,
+    });
+
+    scale.push({
+      name: `${name}-${Math.round(lightness * 100)}`,
+      color: blockColor,
+      isSource: distance(blockColor, sourceColor) < 0.05,
+      sourceColor: sourceColor,
+      matches: findMatches(blockColor),
+    });
+  }
+
+  return scale;
+}
+
+class DesignSystemBuilder {
+  constructor(baseColors) {
+    this.baseColors = baseColors;
+    this.scales = baseColors.map((color) => newScale(color, "ex"));
+  }
+}
+
+function ColorScale({ color, name }) {
+  const scale = newScale(color, name);
+
+  const renderBlock = (block) => {
+    const color = block.color;
+
+    return (
+      <div
+        key={color.toHex()}
+        style={{
+          color: color.isLight() ? "#212121" : "#ededed",
+          backgroundColor: `${color.toHexString()}`,
+          width: "150px",
+          padding: "8px",
+        }}
+      >
+        {color.toHexString()} <br />
+        {block.name}
+        <br />
+        {block.matches.map((c) => (
+          <div>{c.name}</div>
+        ))}
+        <br />
+        {block.isSource ? "origin:" + block.sourceColor.toHexString() : ""}
+      </div>
+    );
+  };
+
+  return <div style={{ margin: "2px" }}>{scale.map(renderBlock)}</div>;
+}
 
 function App() {
+  let colors = [...base, ...charts, ...saturated];
+
+  colors = colors.sort((a, b) => {
+    let ca = tinycolor(a);
+    let cb = tinycolor(b);
+    return ca.toHsl().h - cb.toHsl().h;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ flexDirection: "row", display: "flex" }}>
+      {colors.map((c) => (
+        <ColorScale color={c} name="blue" />
+      ))}
     </div>
   );
 }
---
+++ src/input.js
@@ -0,0 +1,651 @@
+export const showcase = [
+  {
+    id: 1,
+    color: "#FFFFFF",
+    name: "$brand-color-white",
+  },
+  {
+    id: 2,
+    color: "#FBFBFB",
+    name: "$theme-color-supersuperlight",
+  },
+  {
+    id: 3,
+    color: "#F8F8F8",
+    name: "$brand-color-gallery (MultiColorIcon)",
+  },
+  {
+    id: 4,
+    color: "#F7F7F7",
+    name: "$theme-color-superlight",
+  },
+  {
+    id: 5,
+    color: "#F5F5F5",
+    name: "$neutral-lightest",
+  },
+  {
+    id: 6,
+    color: "#F0F0F0",
+    name:
+      "$brand-color-gallery-accent|$ui-border-color-light|$grid-cell-border-color|$grid-sheet-header-border-color",
+  },
+  {
+    id: 7,
+    color: "#EEEEEE",
+    name: "$brand-color-mercury",
+  },
+  {
+    id: 8,
+    color: "#EBEBEB",
+    name:
+      "$neutral-lighter (MultiColorIcon)|$theme-color-light|$theme-base-color-light",
+  },
+  {
+    id: 9,
+    color: "#E4E4E4",
+    name: "$neutral-light",
+  },
+  {
+    id: 10,
+    color: "#D5D5D5",
+    name: "$theme-color-midlight|$ui-border-color-light",
+  },
+  {
+    id: 11,
+    color: "#D2D3D4",
+    name: "$balance-card-secondary-color",
+  },
+  {
+    id: 12,
+    color: "#CECECE",
+    name: "$theme-text-color-superlight",
+  },
+  {
+    id: 13,
+    color: "#CCCCCC",
+    name: "$brand-color-silver",
+  },
+  {
+    id: 14,
+    color: "#BBBBBB",
+    name: "$brand-color-nobel (MultiColorIcon)",
+  },
+  {
+    id: 15,
+    color: "#999999",
+    name: "$brand-color-dustygray",
+  },
+  {
+    id: 16,
+    color: "#888888",
+    name: "$grid-auto-calculation-bar",
+  },
+  {
+    id: 17,
+    color: "#808080",
+    name: "$brand-color-gray",
+  },
+  {
+    id: 18,
+    color: "#757575",
+    name: "$brand-color-boulder",
+  },
+  {
+    id: 19,
+    color: "#6A6A6A",
+    name: "$neutral-dark",
+  },
+  {
+    id: 20,
+    color: "#666666",
+    name: "$brand-color-dovegray",
+  },
+  {
+    id: 21,
+    color: "#5B5B5B",
+    name: "$brand-color-scorpion",
+  },
+  {
+    id: 22,
+    color: "#555555",
+    name: "$theme-color-dark|$theme-text-color-midlight",
+  },
+  {
+    id: 23,
+    color: "#444444",
+    name: "$brand-color-tundora",
+  },
+  {
+    id: 24,
+    color: "#3C3C3C",
+    name: "$theme-color-superdark",
+  },
+  {
+    id: 25,
+    color: "#333333",
+    name: "$brand-color-mine",
+  },
+  {
+    id: 26,
+    color: "#111111",
+    name: "$theme-text-color",
+  },
+  {
+    id: 27,
+    color: "#000000",
+    name: "$brand-color-black",
+  },
+  {
+    id: 28,
+    color: "#FFF0ED",
+    name: "$brand-color-forget-me-not (MultiColorIcon)",
+  },
+  {
+    id: 29,
+    color: "#FAE6E6",
+    name: "$brand-color-we-peep",
+  },
+  {
+    id: 30,
+    color: "#FFD4CA",
+    name: "$brand-color-tuft-bush (MultiColorIcon)",
+  },
+  {
+    id: 31,
+    color: "#FFD3C9",
+    name: "$brand-color-tuft-brush",
+  },
+  {
+    id: 32,
+    color: "#FFCCCC",
+    name: "$brand-color-your-pink",
+  },
+  {
+    id: 33,
+    color: "#FF6C4C",
+    name: "$brand-color-flamingo (MultiColorIcon, Charts)",
+  },
+  {
+    id: 34,
+    color: "#F05028",
+    name: "$brand-color-cinnabar",
+  },
+  {
+    id: 35,
+    color: "#F44336",
+    name: "$theme-color-danger",
+  },
+  {
+    id: 36,
+    color: "#FF050F",
+    name: "$brand-color-red-icon",
+  },
+  {
+    id: 37,
+    color: "#FF0000",
+    name: "$brand-color-red",
+  },
+  {
+    id: 38,
+    color: "#EC1E25",
+    name: "$brand-color-alizarincrimson",
+  },
+  {
+    id: 39,
+    color: "#D40F0F",
+    name: "$accent-6",
+  },
+  {
+    id: 40,
+    color: "#CC0000",
+    name: "$brand-color-guardsmanred",
+  },
+  {
+    id: 41,
+    color: "#B2361A",
+    name: "$brand-color-thunderbird",
+  },
+  {
+    id: 42,
+    color: "#9B3819",
+    name: "$brand-color-cognac",
+  },
+  {
+    id: 43,
+    color: "#8B0000",
+    name: "$showcase-basic-font-color",
+  },
+  {
+    id: 44,
+    color: "#FFF9E6",
+    name: "$brand-color-early-dawn (MultiColorIcon)",
+  },
+  {
+    id: 45,
+    color: "#FFFCCC",
+    name: "$brand-color-lemon",
+  },
+  {
+    id: 46,
+    color: "#FFEEB8",
+    name: "$brand-color-colonial-white (MultiColorIcon)",
+  },
+  {
+    id: 47,
+    color: "#FFE8AB",
+    name: "$brand-color-cape-honey",
+  },
+  {
+    id: 48,
+    color: "#FFC50D",
+    name: "$brand-color-supernova (MultiColorIcon, Charts)",
+  },
+  {
+    id: 49,
+    color: "#FFB428",
+    name: "$brand-color-mysin",
+  },
+  {
+    id: 50,
+    color: "#FFA600",
+    name: "$brand-color-orange",
+  },
+  {
+    id: 51,
+    color: "#F39600",
+    name: "$brand-color-pizazz",
+  },
+  {
+    id: 52,
+    color: "#CD7E00",
+    name: "$brand-color-indochine",
+  },
+  {
+    id: 53,
+    color: "#F3F9EC",
+    name: "$brand-color-loafer",
+  },
+  {
+    id: 54,
+    color: "#F3F9EB",
+    name: "$brand-color-frost (MultiColorIcon)",
+  },
+  {
+    id: 55,
+    color: "#E7F2EC",
+    name: "$brand-color-harp (MultiColorIcon)",
+  },
+  {
+    id: 56,
+    color: "#E1F0DB",
+    name: "$brand-color-zanah",
+  },
+  {
+    id: 57,
+    color: "#DDEEC7",
+    name: "$brand-color-chrome-white (MultiColorIcon)",
+  },
+  {
+    id: 58,
+    color: "#BBDAC7",
+    name: "$brand-color-surf (MultiColorIcon)",
+  },
+  {
+    id: 59,
+    color: "#30CEBB",
+    name: "$brand-color-seafoam",
+  },
+  {
+    id: 60,
+    color: "#8BC53F",
+    name: "$brand-color-sushi (MultiColorIcon, Charts)",
+  },
+  {
+    id: 61,
+    color: "#6BB34D",
+    name: "$brand-color-mantis",
+  },
+  {
+    id: 62,
+    color: "#089C85",
+    name: "$brand-color-fern",
+  },
+  {
+    id: 63,
+    color: "#568F3D",
+    name: "$brand-color-apple",
+  },
+  {
+    id: 64,
+    color: "#008585",
+    name: "$brand-color-teal2",
+  },
+  {
+    id: 65,
+    color: "#188041",
+    name: "$brand-color-jewel (MultiColorIcon, Charts)",
+  },
+  {
+    id: 66,
+    color: "#0E813E",
+    name: "$brand-color-salem",
+  },
+  {
+    id: 67,
+    color: "#4E6B61",
+    name: "$brand-color-cutty-sark",
+  },
+  {
+    id: 68,
+    color: "#065B4E",
+    name: "$brand-color-teal",
+  },
+  {
+    id: 69,
+    color: "#005151",
+    name: "$brand-color-pinegreen",
+  },
+  {
+    id: 70,
+    color: "#F8FCFF",
+    name: "$brand-color-alice-blue (MultiColorIcon)",
+  },
+  {
+    id: 71,
+    color: "#F5FAFE",
+    name: "$brand-color-selago",
+  },
+  {
+    id: 72,
+    color: "#F5F7FB",
+    name: "$brand-color-white-lilac",
+  },
+  {
+    id: 73,
+    color: "#F3FAFD",
+    name: "$theme-color-supersuperlight",
+  },
+  {
+    id: 74,
+    color: "#EAF4FF",
+    name: "$brand-color-zumthor",
+  },
+  {
+    id: 75,
+    color: "#E8F4FD",
+    name: "$brand-color-hawkesblue",
+  },
+  {
+    id: 76,
+    color: "#E1F5FE",
+    name: "$theme-color-superlight",
+  },
+  {
+    id: 77,
+    color: "#E5EBF4",
+    name: "$brand-color-catskill-white",
+  },
+  {
+    id: 78,
+    color: "#DAEAF6",
+    name: "$brand-color-link-water (MultiColorIcon)",
+  },
+  {
+    id: 79,
+    color: "#CFE9FC",
+    name: "$brand-color-lightcyan",
+  },
+  {
+    id: 80,
+    color: "#CAE8F7",
+    name: "$brand-color-tropical-blue (MultiColorIcon)",
+  },
+  {
+    id: 81,
+    color: "#DBE4F0",
+    name: "$brand-color-botticelli",
+  },
+  {
+    id: 82,
+    color: "#D9E2EB",
+    name: "$brand-color-botticelli-2",
+  },
+  {
+    id: 83,
+    color: "#C8E7F6",
+    name: "$brand-color-mint-tulip",
+  },
+  {
+    id: 84,
+    color: "#B7D7ED",
+    name: "$brand-color-spindle (MultiColorIcon)",
+  },
+  {
+    id: 85,
+    color: "#9ED4FA",
+    name: "$brand-color-sail (Charts)",
+  },
+  {
+    id: 86,
+    color: "#51C5FD",
+    name: "$grid-auto-calculation-bar",
+  },
+  {
+    id: 87,
+    color: "#3EBEDE",
+    name: "$accent-0",
+  },
+  {
+    id: 88,
+    color: "#49B0E4",
+    name: "$brand-color-picton-blue (MultiColorIcon, Charts)",
+  },
+  {
+    id: 89,
+    color: "#03A9F4",
+    name: "$theme-color-midlight|$theme-color-light",
+  },
+  {
+    id: 90,
+    color: "#039BE5",
+    name: "$theme-color-dark",
+  },
+  {
+    id: 91,
+    color: "#0288D1",
+    name: "$theme-color-superdark",
+  },
+  {
+    id: 92,
+    color: "#50829F",
+    name: "$brand-color-wedgewood",
+  },
+  {
+    id: 93,
+    color: "#1A8099",
+    name: "$accent-0-dark",
+  },
+  {
+    id: 94,
+    color: "#0A75C2",
+    name: "$brand-color-lochmara (MultiColorIcon)",
+  },
+  {
+    id: 95,
+    color: "#6363E1",
+    name: "$brand-color-cornflower-blue",
+  },
+  {
+    id: 96,
+    color: "#2F729B",
+    name: "$brand-color-astral",
+  },
+  {
+    id: 97,
+    color: "#4C6D89",
+    name: "$brand-color-japanese-laurel",
+  },
+  {
+    id: 98,
+    color: "#226F9A",
+    name: "$brand-color-jelly-bean",
+  },
+  {
+    id: 99,
+    color: "#125C77",
+    name: "$brand-color-chathams-blue",
+  },
+  {
+    id: 100,
+    color: "#005890",
+    name: "$brand-color-pale-blue",
+  },
+  {
+    id: 101,
+    color: "#1C3F5C",
+    name: "$brand-color-cello",
+  },
+  {
+    id: 102,
+    color: "#0F3C61",
+    name: "$brand-color-deep-blue (Charts)",
+  },
+  {
+    id: 103,
+    color: "#003B62",
+    name: "$brand-color-astronaut-blue",
+  },
+  {
+    id: 104,
+    color: "#FBF6FA",
+    name: "$brand-color-prim-2 (MultiColorIcon)",
+  },
+  {
+    id: 105,
+    color: "#FDEBF2",
+    name: "$brand-color-wisp-pink (MultiColorIcon)",
+  },
+  {
+    id: 106,
+    color: "#F1ECF5",
+    name: "$brand-color-whisper (MultiColorIcon)",
+  },
+  {
+    id: 107,
+    color: "#F3E5F0",
+    name: "$brand-color-prim (MultiColorIcon)",
+  },
+  {
+    id: 108,
+    color: "#FAC6DB",
+    name: "$brand-color-classic-rose (MultiColorIcon)",
+  },
+  {
+    id: 109,
+    color: "#F9C5DA",
+    name: "$brand-color-chantilly",
+  },
+  {
+    id: 110,
+    color: "#D7C7E1",
+    name: "$brand-color-snuff (MultiColorIcon)",
+  },
+  {
+    id: 111,
+    color: "#D6A7CB",
+    name: "$brand-color-blossom (MultiColorIcon, Charts)",
+  },
+  {
+    id: 112,
+    color: "#EE3D83",
+    name: "$brand-color-french-rose (MultiColorIcon, Charts)",
+  },
+  {
+    id: 113,
+    color: "#D9534F",
+    name: "$brand-color-roman",
+  },
+  {
+    id: 114,
+    color: "#BA5583",
+    name: "$brand-color-red-tapestry",
+  },
+  {
+    id: 115,
+    color: "#8F5DC1",
+    name: "$brand-color-electric-violet",
+  },
+  {
+    id: 116,
+    color: "#AD36AD",
+    name: "$brand-color-purple-pizzazz",
+  },
+  {
+    id: 117,
+    color: "#884DB3",
+    name: "$brand-color-studio",
+  },
+  {
+    id: 118,
+    color: "#AE2258",
+    name: "$brand-color-maroon-flush",
+  },
+  {
+    id: 119,
+    color: "#754199",
+    name: "$brand-color-royal-purple (MultiColorIcon, Charts)",
+  },
+  {
+    id: 120,
+    color: "#95004A",
+    name: "$color-raspberry",
+  },
+];
+
+export const saturated = [
+  "#008585",
+  "#006d6d",
+  "#005555",
+  "#6363e1",
+  "#5151b9",
+  "#3f3f90",
+  "#ad36ad",
+  "#8e2c8e",
+  "#6f236f",
+  "#8f5dc1",
+  "#754c9e",
+  "#5c3c7c",
+  "#ba5583",
+  "#99466b",
+  "#773654",
+  "#95004a",
+  "#4c6d89",
+  "#3e5970",
+  "#314658",
+];
+
+export const charts = [
+  "#000000",
+  "#8bc53f",
+  "#188041",
+  "#ffc50d",
+  "#ff6c4c",
+  "#754199",
+  "#ab52b1",
+  "#d6a7cb",
+  "#9ed4fa",
+  "#49b0e4",
+  "#0f3c61",
+  "#ee3d83",
+  "#b51e00",
+];
+
+export const base = [
+  "#0A75C2",
+  "#188041",
+  "#CD0000",
+  "#FFA600",
+  "#089C85",
+  "#F34907",
+];
